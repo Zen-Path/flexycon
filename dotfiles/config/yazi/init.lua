987813afaa20ef6f04352eb246e6edb81d57d2a7
Zen-Path <187199005+Zen-Path@users.noreply.github.com>
2025-04-09 12:20:09 +0300

    style: format using stylua

--- dotfiles/config/yazi/init.lua
+++ dotfiles/config/yazi/init.lua
@@ -1,27 +1,27 @@
 -- Enable to manage files across sessions.
-require("session"):setup {
-	sync_yanked = true,
-}
+require("session"):setup({
+    sync_yanked = true,
+})
 
 -- Add a full border around the main content.
-require("full-border"):setup {
-	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-	type = ui.Border.ROUNDED,
-}
+require("full-border"):setup({
+    -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+    type = ui.Border.ROUNDED,
+})
 
 -- Show the status of Git file changes as linemode in the file list.
 require("git"):setup()
 
 -- Docs: https://yazi-rs.github.io/docs/tips#symlink-in-status
 function Status:name()
-	local h = self._current.hovered
-	if not h then
-		return ""
-	end
+    local h = self._current.hovered
+    if not h then
+        return ""
+    end
 
     local linked = ""
     if h.link_to ~= nil then
-    	linked = " -> " .. tostring(h.link_to)
+        linked = " -> " .. tostring(h.link_to)
     end
     return ui.Line(" " .. h.name .. linked)
 end
