308489e789f4ec9a2705bd43efc8d26407ed7a69
Zen-Path <work.gritco@icloud.com>
2025-04-23 17:43:49 +0300

    chore: move ushortcuts to user_shortcuts

--- dotfiles/scripts/user_shortcuts/run.sh
+++ dotfiles/scripts/user_shortcuts/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-base_dir="$FLEXYCON_HOME/dotfiles/scripts/ushortcuts"
+base_dir="$FLEXYCON_HOME/dotfiles/scripts/user_shortcuts"
 "$base_dir/venv/bin/python" "$base_dir/main.py" "$@"
