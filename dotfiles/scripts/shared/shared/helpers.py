4a47634f6025672ea8e0032e2f1d58de626f311f
Zen-Path <work.gritco@icloud.com>
2025-04-24 17:00:31 +0300

    refactor: move run_command func to shared

--- dotfiles/scripts/shared/shared/helpers.py
+++ dotfiles/scripts/shared/shared/helpers.py
@@ -1,7 +1,35 @@
+import logging
 import os
-from typing import List
+import subprocess
+from typing import List, Tuple
+
+logger = logging.getLogger(__name__)
 
 
 def resolve_path(path_parts: List[str]) -> str:
     """Resolve a list of path parts into a single expanded string path."""
     return os.path.expandvars(os.path.join(*path_parts))
+
+
+def run_command(command: List[str]) -> Tuple[int, List[str]]:
+    """Run a shell command."""
+    logger.info(f"Running: {command}")
+    print(command)
+
+    output = []
+    with subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        universal_newlines=True,
+        bufsize=1,
+    ) as process:
+        if process.stdout is not None:
+            for line in process.stdout:
+                output.append(line)
+                logger.info(line.strip())
+
+        returncode = process.wait()
+
+    logger.info(f"Command ({command}) finished with return code {returncode}")
+    return returncode, output
