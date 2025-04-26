20368e6d05dbc8a4a86bee1c548e7b0ba2e67819
Zen-Path <work.gritco@icloud.com>
2025-04-26 18:07:21 +0300

    docs(alias): update var expansion comment

--- dotfiles/config/shell/aliases.sh
+++ dotfiles/config/shell/aliases.sh
@@ -2,14 +2,15 @@
 
 # {{@@ header() @@}}
 
+# shellcheck disable=SC2139
+# Keep env vars in single quotes, so they are expanded when they're used,
+# not when they're defined.
+
 # Make some system commands not require sudo.
 for command in mount umount su shutdown reboot; do
     alias "$command=sudo $command"
 done
 unset command
-
-# Note: Prefer to keep env vars in single quotes, so they are expanded
-# when they're used, not when defined. See SC2139.
 
 # Abbreviations
 alias e='$EDITOR'
