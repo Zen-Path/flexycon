bdb781b7ee16d5513cac66e884747c42aa0abd9e
Zen-Path <work.gritco@icloud.com>
2025-04-26 19:05:16 +0300

    feat: add jsonc_to_json script

---
+++ dotfiles/scripts/jsonc_to_json/main.py
@@ -0,0 +1,16 @@
+import json
+import sys
+
+import commentjson
+
+if len(sys.argv) != 3:
+    print("Usage: python jsonc2json.py input.jsonc output.json")
+    sys.exit(1)
+
+inp, outp = sys.argv[1], sys.argv[2]
+
+with open(inp, "r") as f:
+    data = commentjson.load(f)
+
+with open(outp, "w") as f:
+    json.dump(data, f, indent=4)
