a59d2fc5e1371cbf6e835b4baa8372fea9505f21
Zen-Path <zen.path@tuta.com>
2025-06-24 16:46:45 +0300

    cf(ushort): add /Volumes

--- dotfiles/src/scripts/user_shortcuts/src/data.py
+++ dotfiles/src/scripts/user_shortcuts/src/data.py
@@ -290,4 +290,10 @@ shortcuts: List[Bookmark] = [
         aliases={"default": ["m", "n"], YAZI.name: ["M"]},
         description="mount",
     ),
+    Bookmark(
+        type="d",
+        path_parts=["/Volumes"],
+        aliases={"default": ["v", "o", "l"], YAZI.name: ["V"]},
+        description="volumes",
+    ),
 ]
