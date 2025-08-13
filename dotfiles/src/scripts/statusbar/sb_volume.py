9781804bb654b1e24cbb0741bee083fb3576f8eb
Zen-Path <work.gritco@icloud.com>
2025-08-13 17:34:27 +0300

    refactor(sb): rewrite sb-volume

--- dotfiles/src/scripts/statusbar/sb_volume.py
+++ dotfiles/src/scripts/statusbar/sb_volume.py
@@ -4,58 +4,79 @@ import os
 import subprocess
 import sys
 
+from common.helpers import notify, run_command
 from scripts.statusbar.shared import EDITOR, STATUSBAR, TERMINAL, MouseButton
 
 
-def handle_block_button(button_id):
+def update_volume(amount: int):
+    run_command(
+        [
+            "wpctl",
+            "set-volume",
+            "@DEFAULT_SINK@",
+            f"{abs(amount)}%{'-' if amount < 0 else '+'}",
+        ]
+    )
+
+
+def toggle_mute():
+    run_command(["wpctl", "set-mute", "@DEFAULT_SINK@", "toggle"])
+
+
+ACTIONS = {
+    MouseButton.LEFT: lambda: (
+        run_command(["setsid", "-w", "-f", TERMINAL, "-e", "pulsemixer"]),
+        run_command(["pkill", f"-RTMIN+10", STATUSBAR]),
+    ),
+    MouseButton.MIDDLE: toggle_mute,
+    MouseButton.RIGHT: lambda: notify(
+        "📢 Volume module",
+        "Show sound volume, 🔇 if muted.\n"
+        "\n<b>Actions</b>\n"
+        "- Left click to open 'pulsemixer'\n"
+        "- Middle click to mute\n"
+        "- Right click to show this message\n"
+        "- Scroll to update",
+    ),
+    MouseButton.SCROLL_UP: lambda: update_volume(2),
+    MouseButton.SCROLL_DOWN: lambda: update_volume(-2),
+    MouseButton.EXTRA_3: lambda: run_command([TERMINAL, "-e", EDITOR, __file__]),
+}
+
+
+def handle_block_button():
     """Handle block button events."""
-    match button_id:
-        case MouseButton.LEFT.value:
-            subprocess.Popen(["setsid", "-w", "-f", TERMINAL, "-e", "pulsemixer"])
-            subprocess.run(["pkill", f"-RTMIN+10", STATUSBAR])
-        case MouseButton.MIDDLE.value:
-            subprocess.run(["wpctl", "set-mute", "@DEFAULT_SINK@", "toggle"])
-        case MouseButton.RIGHT.value:
-            subprocess.run(
-                [
-                    "notify-send",
-                    "📢 Volume module",
-                    "Show volume, 🔇 if muted.\n\n<b>Actions</b>\n- Middle click to mute\n- Scroll to change",
-                ]
-            )
-        case MouseButton.EXTRA_1.value:
-            subprocess.run(["wpctl", "set-volume", "@DEFAULT_SINK@", "1%+"])
-        case MouseButton.EXTRA_2.value:
-            subprocess.run(["wpctl", "set-volume", "@DEFAULT_SINK@", "1%-"])
-        case MouseButton.EXTRA.value:
-            subprocess.Popen([TERMINAL, "-e", EDITOR, __file__])
+    block_button = os.environ.get("BLOCK_BUTTON")
+    if not block_button:
+        return
+
+    button = MouseButton(int(block_button))
+    action = ACTIONS.get(button)
+    if action:
+        action()
 
 
 def main():
-    block_button = os.environ.get("BLOCK_BUTTON")
-    if block_button:
-        handle_block_button(int(block_button))
+    handle_block_button()
 
-    # Get volume info
-    get_volume_result = subprocess.check_output(
-        ["wpctl", "get-volume", "@DEFAULT_AUDIO_SINK@"], text=True
-    ).strip()
+    # Should return something like this:
+    # - 'Volume: 0.55'
+    # - 'Volume: 0.50 [MUTED]'
+    volume_info = run_command(
+        ["wpctl", "get-volume", "@DEFAULT_AUDIO_SINK@"]
+    ).output.strip()
 
-    # Extract numeric part after 'Volume: '
-    volume_float = get_volume_result.replace("Volume: ", "").split()[0]
-    volume_int = round(float(volume_float) * 100)
+    volume_int = round(float(volume_info.split(" ")[1]) * 100)
 
-    # Choose icon
-    if volume_int >= 70:
-        icon = "🔊"
-    elif volume_int >= 30:
-        icon = "🔉"
-    else:
-        icon = "🔈"
-
-    # Check mute status
-    if "[MUTED]" in get_volume_result:
+    if "[MUTED]" in volume_info:
         icon = "🔇"
+    else:
+        if volume_int >= 70:
+            icon = "🔊"
+        elif volume_int >= 30:
+            icon = "🔉"
+        else:
+            icon = "🔈"
 
     print(f"{icon}{volume_int}%")
 
