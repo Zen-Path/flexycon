1501d0db751780082614e99b1a2f9ff694fc1132
Zen-Path <work.gritco@icloud.com>
2025-08-26 18:31:29 +0300

    feat(script): allow request logging for media_server

--- dotfiles/src/scripts/media_server/main.py
+++ dotfiles/src/scripts/media_server/main.py
@@ -1,8 +1,10 @@
 import argparse
+import json
 import logging
 
-from common.logger import setup_logging
-from flask import Flask, jsonify, request
+from colorama import Fore, Style
+from common.logger import logger, setup_logging
+from flask import Flask, g, jsonify, request
 from flask_cors import CORS
 from scripts.media_server.src.history import log_history_entry
 from scripts.media_server.src.models import Gallery
@@ -10,6 +12,56 @@ from scripts.media_server.src.models import Gallery
 # Flask setup
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
+
+
+def skip_logging(f):
+    """Decorator to mark a route to skip logging."""
+    f._skip_logging = True
+    return f
+
+
+@app.before_request
+def log_request():
+    view_func = app.view_functions.get(request.endpoint)
+    if getattr(view_func, "_skip_logging", False):
+        g.skip_logging = True
+        return
+
+    params = request.args.to_dict()
+    try:
+        data = request.get_json(silent=True)
+    except Exception:
+        data = None
+
+    if data is None:
+        if request.form:
+            data = request.form.to_dict()
+        else:
+            data = request.data.decode("utf-8") if request.data else None
+
+    logger.info(
+        f"""{Fore.LIGHTBLUE_EX}REQUEST{Fore.LIGHTBLACK_EX}:
+params: {json.dumps(params, indent=4)}
+data: {json.dumps(data, indent=4) if isinstance(data, (dict, list)) else data}
+{Style.RESET_ALL}"""
+    )
+
+
+@app.after_request
+def log_response(response):
+    if getattr(g, "skip_logging", False):
+        return response
+
+    try:
+        json_data = response.get_json()
+        json_fmt = json.dumps(json_data, indent=4, ensure_ascii=False)
+    except Exception:
+        json_fmt = response.get_data(as_text=True)
+
+    logger.info(
+        f"{Fore.LIGHTYELLOW_EX}RESPONSE{Fore.LIGHTBLACK_EX}:\n{json_fmt}{Style.RESET_ALL}"
+    )
+    return response
 
 
 @app.route("/downloadMedia", methods=["POST"])
