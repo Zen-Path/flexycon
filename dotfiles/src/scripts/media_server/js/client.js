7cde5bf0c4b768965753d8b667cc9ffd8336cf9a
Zen-Path <work.gritco@icloud.com>
2025-08-26 18:32:53 +0300

    refactor(script): refactor client side result logging

--- dotfiles/src/scripts/media_server/js/client.js
+++ dotfiles/src/scripts/media_server/js/client.js
@@ -2,7 +2,7 @@
 // @name            File Downloader
 // @namespace       User Scripts
 // @match           http*://*/*
-// @version         1.3.0
+// @version         1.3.9
 // @author          Me
 // @description     Send a download request for a URL to a local media server
 // @downloadURL     ***
@@ -22,32 +22,35 @@ function downloadMedia(urls, type) {
         url: `http://localhost:${SERVER_PORT}/downloadMedia`,
         headers: { "Content-Type": "application/json" },
         data: requestData,
-        onload: function (response) {
-            try {
-                console.log("response.status=", response.status);
-                const data = JSON.parse(response.responseText);
-
-                if (response.status === 200) {
-                    console.log(
-                        `:: Download output:
-${Array.isArray(data.output) ? data.output.join("") : data.output}`
-                    );
-                    alert("Download complete!");
-                } else {
-                    console.error(
-                        `:: Response status: ${response.status}`,
-                        data.error
-                    );
-                    alert("Download failed.");
-                }
-            } catch (err) {
-                console.error(":: Media server", err);
-                alert("Download failed.");
-            }
-        },
         onerror: function (error) {
-            console.error(":: Media server", error);
+            console.error(":: Download failed", error);
             alert("Download failed.");
+        },
+        onload: function (response) {
+            const data = JSON.parse(response.responseText);
+
+            const output_fmt = data.output
+                .replace(/\u001b\[[0-9;]*m/g, "")
+                .trim();
+
+            console.log(
+                ":: Download info",
+                `
+- request_status: ${response.status}
+- process_return_code: ${data.return_code}
+- output:\n${output_fmt}`
+            );
+
+            if (
+                data.return_code !== 0 ||
+                response.status !== 200 ||
+                output_fmt.includes("[ytdl][error]")
+            ) {
+                alert("Download failed.");
+                return;
+            }
+
+            alert("Download successful!");
         },
     });
 }
