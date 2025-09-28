3dd7ff2968b52fd7eedf48daf45abf4e07f59fdc
ZenPath <zen.path@tuta.com>
2025-09-28 18:28:15 +0300

    cf(yazi): add starship plugin and config

--- dotfiles/config/yazi/init.lua
+++ dotfiles/config/yazi/init.lua
@@ -25,3 +25,14 @@ function Status:name()
     end
     return ui.Line(" " .. h.name .. linked)
 end
+
+-- Enable the starship integration
+require("starship"):setup({
+    -- Hide flags (such as filter, find and search). This is recommended for starship themes which
+    -- are intended to go across the entire width of the terminal.
+    hide_flags = false, -- Default: false
+    -- Whether to place flags after the starship prompt. False means the flags will be placed before the prompt.
+    flags_after_prompt = true, -- Default: true
+    -- Custom starship configuration file to use
+    config_file = "~/.config/starship/yazi_cfg.toml", -- Default: nil
+})
