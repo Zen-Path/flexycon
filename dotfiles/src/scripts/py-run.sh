6d85c8bf84815891287b27f5898d6e1b12da7af1
ZenPath <zen.path@tuta.com>
2025-10-03 15:07:39 +0300

    style(script): update error msgs of py-run

--- dotfiles/src/scripts/py-run.sh
+++ dotfiles/src/scripts/py-run.sh
@@ -5,12 +5,12 @@ PYTHON="$PROJECT_DIR/venv/bin/python"
 TARGET_SCRIPT="$PROJECT_DIR/dotfiles/src/scripts/{{@@ _dotfile_abs_dst.split('/')[-1] @@}}/main.py"
 
 if [ ! -x "$PYTHON" ]; then
-    echo "Error: Python not found at $PYTHON" >&2
+    echo "Error: Python not found at '$PYTHON'" >&2
     exit 1
 fi
 
 if [ ! -f "$TARGET_SCRIPT" ]; then
-    echo "Error: Script not found: $TARGET_SCRIPT" >&2
+    echo "Error: Script not found at '$TARGET_SCRIPT'" >&2
     exit 1
 fi
 
