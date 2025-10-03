2b8d93f1307f53b64fdc34e0294a7e319d7ef7a3
ZenPath <zen.path@tuta.com>
2025-10-03 21:32:49 +0300

    feat(script): add render specifying option to user_shortcuts

--- dotfiles/src/scripts/user_shortcuts/main.py
+++ dotfiles/src/scripts/user_shortcuts/main.py
@@ -6,12 +6,22 @@ from scripts.user_shortcuts.data.shortcuts import shortcuts
 from scripts.user_shortcuts.src.formatting import format_bookmarks
 from scripts.user_shortcuts.src.renderers import NVIM, YAZI, ZSH
 
+AVAILABLE_RENDERERS = [ZSH, NVIM, YAZI]
+
 
 def build_parser():
     parser = argparse.ArgumentParser(
         description="Generate bookmarks for various tools."
     )
 
+    parser.add_argument(
+        "-r",
+        "--renderer",
+        choices=sorted(
+            [renderer.name.lower() for renderer in AVAILABLE_RENDERERS], key=str.lower
+        ),
+        help="generate bookmarks only for a renderer",
+    )
     parser.add_argument(
         "-l",
         "--list-shortcuts",
@@ -32,7 +42,13 @@ def main():
         print(format_bookmarks(shortcuts))
         return
 
-    for renderer in (ZSH, NVIM, YAZI):
+    if args.renderer:
+        for renderer in AVAILABLE_RENDERERS:
+            if renderer.name.lower() == args.renderer:
+                renderer.process(shortcuts)
+        return
+
+    for renderer in AVAILABLE_RENDERERS:
         renderer.process(shortcuts)
 
 
