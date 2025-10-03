31da0899530f6dc25654a675114a6725af47d4de
ZenPath <zen.path@tuta.com>
2025-10-03 21:51:19 +0300

    refactor: use dotfile key for py-run

--- dotfiles/bin/py-run.sh
+++ dotfiles/bin/py-run.sh
@@ -2,7 +2,7 @@
 
 PROJECT_DIR="$FLEXYCON_HOME"
 PYTHON="$PROJECT_DIR/venv/bin/python"
-TARGET_SCRIPT="$PROJECT_DIR/dotfiles/src/scripts/{{@@ _dotfile_abs_dst.split('/')[-1] @@}}/main.py"
+TARGET_SCRIPT="$PROJECT_DIR/dotfiles/src/scripts/{{@@ _dotfile_key.lstrip("d_").lstrip("f_") @@}}/main.py"
 
 if [ ! -x "$PYTHON" ]; then
     echo "Error: Python not found at '$PYTHON'" >&2
