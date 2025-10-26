7045ec3dfb71c0cdf78f6ab9f481b0471f251e76
Zen-Path <zen.path@tuta.com>
2025-10-26 23:03:49 +0200

    refactor(bash): include aliases instead of sourcing

--- dotfiles/config/bash/profile.sh
+++ dotfiles/config/bash/profile.sh
@@ -4,9 +4,8 @@ if [ -f ~/.bashrc ]; then
     source ~/.bashrc
 fi
 
-[ -f "$XDG_CONFIG_HOME/shell/aliases.sh" ] && source "$XDG_CONFIG_HOME/shell/aliases.sh"
-
-echo "$XDG_CONFIG_HOME/shell/aliases.sh"
+# ALIASES
+# {%@@ include 'config/shell/aliases.sh' @@%}
 
 # USER FUNCTIONS
 # {%@@ include 'config/shell/functions.sh' @@%}
