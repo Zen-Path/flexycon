2d30bc6853ac4e4c918c554ae9958d1c264d9a71
ZenPath <zen.path@tuta.com>
2025-11-09 16:40:38 +0200

    refactor: rewrite jsonc_to_json to be up to standard

--- dotfiles/src/scripts/jsonc_to_json/main.py
+++ dotfiles/src/scripts/jsonc_to_json/main.py
@@ -2,19 +2,51 @@
 
 # {{@@ header() @@}}
 
+import argparse
 import json
-import sys
+import logging
 
 import commentjson
+from common.logger import logger, setup_logging
 
-if len(sys.argv) != 3:
-    print("Usage: python jsonc2json.py input.jsonc output.json")
-    sys.exit(1)
 
-inp, outp = sys.argv[1], sys.argv[2]
+def build_parser():
+    parser = argparse.ArgumentParser(description="Convert a jsonc file to json.")
 
-with open(inp, "r") as f:
-    data = commentjson.load(f)
+    parser.add_argument(
+        "input_file",
+        help="the jsonc file",
+    )
 
-with open(outp, "w") as f:
-    json.dump(data, f, indent=4)
+    parser.add_argument(
+        "output_file",
+        help="the resulting json file",
+    )
+
+    parser.add_argument(
+        "-i",
+        "--indentation",
+        type=int,
+        default=4,
+        help=("specify the number of spaces used for the json file's indentation"),
+    )
+
+    parser.add_argument("--verbose", action="store_true", help="enable debug output")
+
+    return parser
+
+
+def main():
+    args = build_parser().parse_args()
+
+    setup_logging(logger, logging.DEBUG if args.verbose else logging.WARNING)
+
+    with open(args.input_file, "r") as f:
+        data = commentjson.load(f)
+
+    with open(args.output_file, "w") as f:
+        json.dump(data, f, indent=args.indentation)
+
+
+if __name__ == "__main__":
+    main()
