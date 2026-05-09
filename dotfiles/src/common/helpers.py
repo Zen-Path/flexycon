068a9b5310e43e64f003c3d47c0f75cc5b4f235b
ZenPath <zen.path@tuta.com>
2026-05-09 14:12:19 +0300

    feat: move ScreenshotUtility and Maim, small rewrite of screenshot_utility

--- dotfiles/src/common/helpers.py
+++ dotfiles/src/common/helpers.py
@@ -8,6 +8,7 @@ import sys
 import tomllib
 import unicodedata
 from dataclasses import dataclass
+from datetime import datetime
 from pathlib import Path
 from typing import Callable, Literal, Sequence
 
@@ -666,3 +667,154 @@ class Window:
             return None
 
         return result.output
+
+
+class Maim:
+    @classmethod
+    def run(
+        cls,
+        output_path: Path,
+        select: bool = False,
+        border_size: float = 4.0,
+        padding: float = 0.0,
+        tolerance: float = 20.0,
+        color: Color = Color(100.0, 0.0, 0.0, 0.3),
+        no_decorations: int = 2,
+        hide_cursor: bool = True,
+        quiet: bool = True,
+        delay: float = 0.2,
+        window: Window | None = None,
+    ) -> bool:
+        """
+        Take screenshot of desktop and save to an image.
+        """
+        cmd = [
+            "maim",
+            f"--bordersize={border_size}",
+            f"--padding={padding}",
+            f"--tolerance={tolerance}",
+            f"--color={color}",
+            f"--nodecorations={no_decorations}",
+        ]
+        if hide_cursor:
+            cmd.append("--hidecursor")
+        if quiet:
+            cmd.append("--quiet")
+        if window:
+            cmd.extend(["--window", str(window.id), "--capturebackground"])
+
+        if select:
+            cmd.extend(["--select", "--nodrag", "--highlight"])
+
+        cmd.extend(["--delay", str(delay)])
+        cmd.append(str(output_path))
+
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+
+        return run_command(cmd).success
+
+
+class ScreenshotUtility:
+    OUTPUT_DIR = (
+        Path(os.getenv("XDG_PICTURES_DIR") or Path.home() / "Pictures") / "Screenshots"
+    )
+
+    @classmethod
+    def _capture(
+        cls,
+        output_path: Path,
+        copy_output: bool,
+        select: bool = False,
+        window: Window | None = None,
+    ):
+        success = Maim.run(output_path, select, window=window)
+
+        if success and copy_output:
+            # TODO: temporary, until sorting common logic structure to avoid circular
+            # dependencies
+            from common.packages.clipboard_utilities import copy_file
+
+            copy_file(output_path)
+            NotificationSystem.run(
+                "Screenshot captured",
+                f"Saved at {str(output_path)!r}",
+                icon_path=output_path,
+                open_image_onclick=True,
+            )
+
+        return success
+
+    @classmethod
+    def _compose_output_path(
+        cls,
+        capture_type: Literal["area", "window", "screen", "full"],
+        output_dir: Path | None = None,
+        name: str | None = None,
+        ext: str = "png",
+        include_timestamp: bool = True,
+    ) -> Path:
+        """Generate a file path to the output directory with the current timestamp."""
+        name_fmt = f"-{name[:100]}" if name else ""
+
+        timestamp_fmt = (
+            datetime.now().strftime("%Y-%m-%d_%H-%M-%S") if include_timestamp else ""
+        )
+
+        filename = "_".join(
+            [part for part in [timestamp_fmt, name_fmt, capture_type] if part]
+        )
+
+        return Path(
+            output_dir or cls.OUTPUT_DIR,
+            f"{filename}.{ext}".lower(),
+        )
+
+    @classmethod
+    def area(cls, output_dir: Path | None = None, copy_output=False) -> bool:
+        """Capture a selected area."""
+        output_path = cls._compose_output_path("area", output_dir=output_dir)
+        return cls._capture(output_path, copy_output, select=True)
+
+    @classmethod
+    def window(
+        cls,
+        window: Window | None = None,
+        include_window_name=False,
+        output_dir: Path | None = None,
+        copy_output=False,
+    ) -> bool:
+        """Capture a window."""
+        if window is None:
+            window = Window.get_active_window()
+
+        # If no window found, capture the screen
+        if window is None:
+            return cls.screen(output_dir=output_dir, copy_output=copy_output)
+
+        window_name = None
+        if include_window_name:
+            window_name = Window.get_window_name(window.id)
+
+        output_path = cls._compose_output_path(
+            "window", name=window_name, output_dir=output_dir
+        )
+
+        return cls._capture(output_path, copy_output, window=window)
+
+    @classmethod
+    def screen(
+        cls,
+        screen: int | None = None,
+        output_dir: Path | None = None,
+        copy_output=False,
+    ) -> bool:
+        """Capture a screen."""
+        # TODO: Implement logic for targeting a single screen
+        output_path = cls._compose_output_path("screen", output_dir=output_dir)
+        return cls._capture(output_path, copy_output)
+
+    @classmethod
+    def full_screen(cls, output_dir: Path | None = None, copy_output=False) -> bool:
+        """Capture all screens."""
+        output_path = cls._compose_output_path("full", output_dir=output_dir)
+        return cls._capture(output_path, copy_output)
--- dotfiles/src/scripts/screenshot_utility/main.py
+++ dotfiles/src/scripts/screenshot_utility/main.py
@@ -4,136 +4,10 @@
 
 import argparse
 import logging
-import os
-import subprocess
-from datetime import datetime
 from pathlib import Path
 
-from common.helpers import Dmenu, NotificationSystem, get_version, run_command
+from common.helpers import Dmenu, ScreenshotUtility, get_version
 from common.logger import logger, setup_logging
-from common.packages.clipboard_utilities import copy_file
-
-
-class MaimUtility:
-    def __init__(
-        self,
-        bordersize=4.0,
-        padding=0.0,
-        tolerance=20.0,
-        color=(100.0, 0.0, 0.0, 0.3),
-        nodecorations=2,
-        hidecursor=True,
-        quiet=True,
-        delay=0.2,
-        window=None,
-    ):
-        self.bordersize = bordersize
-        self.padding = padding
-        self.tolerance = tolerance
-        self.color = color  # Expecting a tuple or list of floats
-        self.nodecorations = nodecorations
-        self.hidecursor = hidecursor
-        self.quiet = quiet
-        self.delay = delay
-        self.window = window
-
-    def _color_to_string(self):
-        """Convert the color array to a string representation."""
-        return ",".join(f"{c:.1f}" for c in self.color)
-
-    def run(self, output_path: Path, select=False) -> bool:
-        """Run maim with the configured options."""
-        cmd = [
-            "maim",
-            f"--bordersize={self.bordersize}",
-            f"--padding={self.padding}",
-            f"--tolerance={self.tolerance}",
-            f"--color={self._color_to_string()}",
-            f"--nodecorations={self.nodecorations}",
-        ]
-        if self.hidecursor:
-            cmd.append("--hidecursor")
-        if self.quiet:
-            cmd.append("--quiet")
-        if self.window:
-            cmd.extend(["--window", self.window, "--capturebackground"])
-
-        if select:
-            cmd.extend(["--select", "--nodrag", "--highlight"])
-
-        cmd.extend(["--delay", str(self.delay)])
-        cmd.append(str(output_path))
-
-        output_path.parent.mkdir(parents=True, exist_ok=True)
-
-        return run_command(cmd).success
-
-
-class ScreenshotUtility:
-    def __init__(self, maim=None, output_dir=None):
-        self.maim = maim or MaimUtility()
-        self.output_dir = os.path.expanduser(
-            output_dir or f"{os.getenv('XDG_PICTURES_DIR', '~/Pictures')}/Screenshots"
-        )
-
-    def _capture(self, output_path: Path, copy_output, select=False):
-        response = self.maim.run(output_path, select)
-
-        if response and copy_output:
-            copy_file(output_path)
-            NotificationSystem.run(
-                "Screenshot captured",
-                f"Saved at {str(output_path)!r}",
-                icon_path=output_path,
-                open_image_onclick=True,
-            )
-
-    def compose_output_path(self, capture_type, name=None, ext="png") -> Path:
-        """Generate a file path to the output directory with the current timestamp."""
-        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        name_fmt = f"-{name[:100].replace(' ', '-')}" if name else ""
-
-        return Path(
-            self.output_dir, f"{timestamp}_{capture_type}{name_fmt}.{ext}".lower()
-        )
-
-    def area(self, copy_output=False):
-        """Capture a selected area."""
-        output_path = self.compose_output_path("area")
-        self._capture(output_path, copy_output, select=True)
-
-    def window(self, window=None, include_window_name=False, copy_output=False):
-        """Capture a window."""
-        # TODO: Implement a proper Window class
-
-        if window is None:
-            window = (
-                subprocess.check_output(["xdotool", "getactivewindow"]).strip().decode()
-            )
-
-        window_name = None
-        if include_window_name:
-            window_name = (
-                subprocess.check_output(["xdotool", "getwindowname", window])
-                .strip()
-                .decode()
-            )
-
-        output_path = self.compose_output_path("window", name=window_name)
-
-        self.maim.window = window
-        self._capture(output_path, copy_output)
-
-    def screen(self, screen=None, copy_output=False):
-        """Capture a screen."""
-        # TODO: Implement logic for targeting a single screen
-        output_path = self.compose_output_path("screen")
-        self._capture(output_path, copy_output)
-
-    def full_screen(self, copy_output=False):
-        """Capture all screens."""
-        output_path = self.compose_output_path("full")
-        self._capture(output_path, copy_output)
 
 
 def prompt_user(actions):
@@ -157,12 +31,12 @@ def build_parser():
     # Area action
     subparsers.add_parser("area", help="Capture a selected area")
 
-    # Window action with additional options
+    # Window action
     window_parser = subparsers.add_parser(
         "window", help="Capture the active or specified window"
     )
     window_parser.add_argument(
-        "-w", "--window-id", type=str, help="Specify a window ID"
+        "-w", "--window-id", type=int, help="Specify a window ID"
     )
     window_parser.add_argument(
         "-n",
@@ -171,37 +45,30 @@ def build_parser():
         help="Include window name in the file name",
     )
 
-    # Screen action with additional options
+    # Screen action
     screen_parser = subparsers.add_parser(
-        "screen", help="Capture the screen where the active window resides"
+        "screen", help="Capture the active or specified screen"
     )
     screen_parser.add_argument(
-        "-s", "--screen-name", type=str, help="Specify the screen name"
+        "-s", "--screen-id", type=int, help="Specify the screen ID"
     )
 
     # Full action
     subparsers.add_parser("full", help="Capture all screens")
 
     # Global options
-    copy_group = parser.add_mutually_exclusive_group()
-    copy_group.add_argument(
-        "--copy",
-        dest="copy",
-        action="store_true",
-        default=True,
-        help="Copy the screenshot to the clipboard (default)",
-    )
-    copy_group.add_argument(
+    parser.add_argument(
         "--no-copy",
-        dest="copy",
+        dest="copy_output",
         action="store_false",
+        default=True,
         help="Do not copy the screenshot to the clipboard",
     )
 
     parser.add_argument(
         "-d",
         "--output-directory",
-        type=str,
+        type=Path,
         default=None,
         help="Set the output directory for screenshots",
     )
@@ -221,17 +88,23 @@ def main():
 
     setup_logging(logger, logging.DEBUG if args.verbose else logging.WARNING)
 
-    screenshot_utility = ScreenshotUtility(
-        output_dir=args.output_directory if args.output_directory else None
-    )
+    output_dir = args.output_directory if args.output_directory else None
+
+    # TODO: unify action handling logic into a registry
 
     if not args.action:
         actions = {
-            "🔳 Area": lambda: screenshot_utility.area(copy_output=args.copy),
-            "🪟 Window": lambda: screenshot_utility.window(copy_output=args.copy),
-            "🖥️ Screen": lambda: screenshot_utility.screen(copy_output=args.copy),
-            "🌍 Full Screen": lambda: screenshot_utility.full_screen(
-                copy_output=args.copy
+            "🔳 Area": lambda: ScreenshotUtility.area(
+                output_dir=output_dir, copy_output=args.copy_output
+            ),
+            "🪟 Window": lambda: ScreenshotUtility.window(
+                output_dir=output_dir, copy_output=args.copy_output
+            ),
+            "🖥️ Screen": lambda: ScreenshotUtility.screen(
+                output_dir=output_dir, copy_output=args.copy_output
+            ),
+            "🌍 Full Screen": lambda: ScreenshotUtility.full_screen(
+                output_dir=output_dir, copy_output=args.copy_output
             ),
         }
         prompt_user(actions)
@@ -239,17 +112,24 @@ def main():
 
     match args.action:
         case "area":
-            screenshot_utility.area(copy_output=args.copy)
+            ScreenshotUtility.area(output_dir=output_dir, copy_output=args.copy_output)
         case "window":
-            screenshot_utility.window(
+            ScreenshotUtility.window(
                 window=args.window_id,
                 include_window_name=args.include_name,
-                copy_output=args.copy,
+                output_dir=output_dir,
+                copy_output=args.copy_output,
             )
         case "screen":
-            screenshot_utility.screen(screen=args.screen_name, copy_output=args.copy)
+            ScreenshotUtility.screen(
+                screen=args.screen_id,
+                output_dir=output_dir,
+                copy_output=args.copy_output,
+            )
         case "full":
-            screenshot_utility.full_screen(copy_output=args.copy)
+            ScreenshotUtility.full_screen(
+                output_dir=output_dir, copy_output=args.copy_output
+            )
         case _:
             raise ValueError(f"Unknown action: {args.action}")
 
