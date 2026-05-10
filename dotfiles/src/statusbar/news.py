1cdd8807adcf42d02c30c46de8f67bf17770aee5
ZenPath <zen.path@tuta.com>
2026-05-10 22:48:16 +0300

    refactor(sb): rewrite news script
    Squashed commit of the following:
    
    commit 06ed75c37c75ef861f8d0121b8eb1d5e8678083a
    Author: ZenPath <zen.path@tuta.com>
    Date:   Sun May 10 21:26:20 2026 +0300
    
        refactor(sb): rewrite news script

--- dotfiles/src/statusbar/news.py
+++ dotfiles/src/statusbar/news.py
@@ -3,17 +3,18 @@
 # {{@@ header() @@}}
 
 import os
-import subprocess
+import shutil
+from pathlib import Path
 
 from common.helpers import NotificationSystem, run_command, run_command_background
+from common.logger import logger
 from common.statusbar import EDITOR, TERMINAL, MouseButton, handle_block_button
 
-# Environment variables
-NEWS_DIR = os.path.join(
-    os.getenv("XDG_DATA_HOME", os.path.expanduser("~/.local/share")), "newsraft"
+NEWS_DIR = (
+    Path(os.getenv("XDG_DATA_HOME") or Path.home() / ".local" / "share") / "newsraft"
 )
-NEWS_DB = os.path.join(NEWS_DIR, "newsraft.sqlite3")
-NEWS_DB_BACKUP = os.path.join(NEWS_DIR, "newsraft_backup.sqlite3")
+NEWS_DB = NEWS_DIR / "newsraft.sqlite3"
+NEWS_DB_BACKUP = NEWS_DIR / "newsraft_backup.sqlite3"
 
 
 def reload_newsraft() -> bool:
@@ -21,22 +22,7 @@ def reload_newsraft() -> bool:
     return run_command(["newsraft", "-e", "reload-all"]).success
 
 
-def get_unread_items_count():
-    unread_count = get_unread_newsraft()
-
-    # If newsraft fails, fallback to reading from the database
-    if not unread_count:
-        try:
-            # TODO: use shutils
-            subprocess.run(["cp", "-f", NEWS_DB, NEWS_DB_BACKUP], check=False)
-            unread_count = get_unread_db(NEWS_DB_BACKUP)
-        except Exception:
-            unread_count = None
-
-    return unread_count
-
-
-def get_unread_newsraft() -> int | None:
+def _get_unread_newsraft() -> int | None:
     """Get unread items count using newsraft."""
     result = run_command(["newsraft", "-e", "print-unread-items-count"])
     if result.success:
@@ -45,39 +31,62 @@ def get_unread_newsraft() -> int | None:
     return None
 
 
-def get_unread_db(db_file):
+def _get_unread_db(db_path: Path) -> int | None:
     """Get unread items count directly from the database."""
     try:
         # TODO: maybe use sqlite module?
-        result = subprocess.run(
+        result = run_command(
             [
                 "sqlite3",
-                f"file:{db_file}?mode=ro",
+                f"file:{db_path}?mode=ro",
                 "SELECT COUNT(*) FROM items WHERE unread = 1",
-            ],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.DEVNULL,
-            text=True,
+            ]
         )
-        return result.stdout.strip()
-    except Exception:
+        if result.success:
+            return int(result.output)
+        return None
+    except Exception as e:
+        logger.error(f"Could not read from db {str(db_path)!r}: {e}")
         return None
 
 
-def handle_reload():
+def get_unread_count() -> int | None:
+    unread_count = _get_unread_newsraft()
+    if unread_count:
+        return unread_count
+
+    # Fallback to reading from the database
+    try:
+        shutil.copy2(NEWS_DB, NEWS_DB_BACKUP)
+        return _get_unread_db(NEWS_DB_BACKUP)
+    except Exception as e:
+        logger.error(
+            f"Failed to backup db {str(NEWS_DB)!r} to {str(NEWS_DB_BACKUP)!r}: {e}"
+        )
+        return None
+
+
+def handle_reload() -> bool:
     """Reload all items and notify user."""
-    NotificationSystem.run("News Update", "Updating news, please wait...")
-    if reload_newsraft():
-        unread_count = get_unread_items_count()
-        if unread_count:
-            NotificationSystem.run("News Update", f"Newsraft has {unread_count} items!")
-        else:
-            NotificationSystem.run(
-                "News Update",
-                "Update succeeded, but unread count could not be retrieved.",
-            )
+    notification_title = "News Fetch"
+    NotificationSystem.run(notification_title, "Fetching news. Please wait...")
+
+    if not reload_newsraft():
+        NotificationSystem.run(notification_title, "Unable to fetch news.")
+        return False
+
+    unread_count = get_unread_count()
+    if unread_count:
+        NotificationSystem.run(
+            notification_title, f"Newsraft has {unread_count} items."
+        )
     else:
-        NotificationSystem.run("News Update Failed", "Unable to update the news.")
+        NotificationSystem.run(
+            notification_title,
+            "Fetch successful, but unknown item count.",
+        )
+
+    return True
 
 
 ACTIONS = {
@@ -100,7 +109,7 @@ ACTIONS = {
 def main():
     handle_block_button(ACTIONS)
 
-    unread_count = get_unread_items_count()
+    unread_count = get_unread_count()
     if not unread_count:
         print(" ❗err")
     else:
