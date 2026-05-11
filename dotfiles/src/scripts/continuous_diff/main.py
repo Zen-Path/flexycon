77249a5d916f89d708c1d060567b00a307386484
ZenPath <zen.path@tuta.com>
2026-05-11 23:11:05 +0300

    refactor(script): rewrite continuous_diff

--- dotfiles/src/scripts/continuous_diff/main.py
+++ dotfiles/src/scripts/continuous_diff/main.py
@@ -2,18 +2,23 @@
 
 # {{@@ header() @@}}
 
+import argparse
 import difflib
+import logging
 import sys
 from pathlib import Path
 
+from common.helpers import get_version
+from common.logger import logger, setup_logging
 
-def read_file(path):
+
+def read_file(path: Path) -> list[str]:
     """Read the contents of the file as a list of lines."""
     with open(path, "r") as file:
         return file.readlines()
 
 
-def display_diff(old, new):
+def display_diff(old: list[str], new: list[str]):
     """Display the diff between the old and new file contents."""
     diff = difflib.unified_diff(
         old, new, fromfile="Previous", tofile="Current", lineterm=""
@@ -21,15 +26,13 @@ def display_diff(old, new):
     print("\n".join(diff) or "No changes detected.")
 
 
-def monitor_file(path):
+def monitor_file(path: Path):
     """Monitor the file for changes and display diffs."""
-    path = Path(path)
-
     if not path.is_file():
-        print(f"Error: {path} is not a valid file.")
-        sys.exit(1)
+        logger.error(f"Path {str(path)!r} is not a valid file.")
+        return
 
-    print(f"Monitoring file: {path}")
+    logger.info(f"Monitoring file {str(path)!r}")
     old_content = read_file(path)
 
     while True:
@@ -39,18 +42,46 @@ def monitor_file(path):
             display_diff(old_content, new_content)
             old_content = new_content
         except FileNotFoundError:
-            print("File not found!")
+            logger.error("File not found!")
         except Exception as e:
-            print(f"An error occurred: {e}")
+            logger.error(f"An error occurred: {e}")
+
+
+def build_parser() -> argparse.ArgumentParser:
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(
+        prog="continuous_diff",
+        description="Showcase diff of the same file as it's being changed.",
+    )
+
+    parser.add_argument(
+        "-p", "--target-path", required=True, type=Path, help="the jsonc file"
+    )
+
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="enable debug output"
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {get_version()}"
+    )
+
+    return parser
+
+
+def main():
+    args = build_parser().parse_args()
+
+    setup_logging(logger, logging.DEBUG if args.verbose else logging.ERROR)
+
+    if args.target_path is None:
+        logger.error("Missing target path.")
+        sys.exit(1)
+
+    try:
+        monitor_file(Path(args.target_path))
+    except KeyboardInterrupt:
+        print("\nExiting...")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python file_diff_tool.py <path_to_file>")
-        sys.exit(1)
-
-    file_path = sys.argv[1]
-    try:
-        monitor_file(file_path)
-    except KeyboardInterrupt:
-        print("\nExiting...")
+    main()
