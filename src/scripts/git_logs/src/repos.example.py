5eb2339251bf16917f7eb1df0c0c24ced06d334a
ZenPath <zen.path@tuta.com>
2026-05-14 19:24:24 +0300

    data: use flexycon instead of dwm in example repos data

--- src/scripts/git_logs/src/repos.example.py
+++ src/scripts/git_logs/src/repos.example.py
@@ -5,5 +5,5 @@ src_dir = "$XDG_SRC_HOME"
 GIT_REPOS = [
     GitRepo("University", ["$HOME", "University"]),
     # Source Repos
-    GitRepo("dwm", [src_dir, "dwm-flexipatch"]),
+    GitRepo("flexycon", [src_dir, "flexycon"]),
 ]
