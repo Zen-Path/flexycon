daf041c03477d0c82d8427da0ab5b442568fd7f5
ZenPath <zen.path@tuta.com>
2026-06-14 18:03:49 +0300

    cf(shell): remove --quit-if-one-screen flag from less

--- dotfiles/config/shell/profile.sh
+++ dotfiles/config/shell/profile.sh
@@ -98,7 +98,7 @@ export BAT_THEME='gruvbox-dark'
 # export PASTEL_COLOR_MODE='24bit'
 
 ## Less
-export LESS='--RAW-CONTROL-CHARS --quit-if-one-screen'
+export LESS='--RAW-CONTROL-CHARS'
 export LESSOPEN='| /usr/bin/highlight -O ansi %s 2>/dev/null'
 
 # {%@@- if os == "windows" +@@%}
